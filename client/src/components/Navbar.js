8e5c387f8ab116f503c22ecc65c51e89d43100c2
VictoriaShyika <victoriashyika@gmail.com>
2022-05-04 16:54:15 +0300

    master: Added Logout button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,14 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context";
 
 export default function Navbar() {
-//   const { isAuth, setIsAuth } = useContext(AuthContext);
+  const { isAuth, setIsAuth } = useContext(AuthContext);
 
   return (
     <nav className="navbar navbar-expand fixed-top navbar-dark bg-dark">
       <div className="container-fluid">
-        <a className="navbar-brand text-uppercase fw-bold">Task Manager</a>
-        {/* <div className="ml-auto">{!isAuth ? <Link to="/login" /> : <Link />}</div> */}
+        <span className="navbar-brand text-uppercase fw-bold">Task Manager</span>
+        <div className="ml-auto">
+          {!isAuth ? (
+            <Link to="/login">
+              <button className="btn btn-outline-light px-4">Login</button>
+            </Link>
+          ) : <button onClick={() => setIsAuth(false)} className="btn btn-outline-light px-4 ">Logout</button>}
+        </div>
       </div>
     </nav>
   );
