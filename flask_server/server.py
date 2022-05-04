aa194a82ba64c27e0134d814dc99a7cbe56e7bad
VictoriaShyika <victoriashyika@gmail.com>
2022-05-04 11:03:03 +0300

    master: Added new function

--- flask_server/server.py
+++ flask_server/server.py
@@ -8,6 +8,9 @@ def members():
     """Login function"""
     return {"token": "dhafkjahlfashfahdalbksdk431243"}
 
+@app.route("/home", methods=["GET"])
+def home():
+    return {"text": "There some text"}
 
 if __name__ == "__main__":
    app.run(debug=True)
