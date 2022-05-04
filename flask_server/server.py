4160790bc23a76b6019c3be00b6169898963cbd6
VitaliiKulinich <vitalii.kulinich1@gmail.com>
2022-05-04 23:14:30 +0300

    master: Removed useless code

--- flask_server/server.py
+++ flask_server/server.py
@@ -8,9 +8,11 @@ def members():
     """Login function"""
     return {"token": "dhafkjahlfashfahdalbksdk431243"}
 
+
 @app.route("/home", methods=["GET"])
 def home():
     return {"text": "There some text"}
+
 
 if __name__ == "__main__":
    app.run(debug=True)
