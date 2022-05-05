b713120c1c4e6dc567ec108fd20d2d17ed2acad1
VitaliiKulinich <vitalii.kulinich1@gmail.com>
2022-05-06 00:12:21 +0300

    master: Added login logic

--- flask_server/server.py
+++ flask_server/server.py
@@ -4,11 +4,19 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-LOGINS = ["app", "hello", "good"]
-PATHWORDS = {
-    "app": "123",
-    "hello": "321",
-    "good": "12345",
+DATABASE = {
+    "users": [{
+        "name": "Vitalii",
+        "email": "ajax@gmail.com",
+        "password": "ajax",
+        "tokens": []
+    }, {
+        "name": "Vika",
+        "email": "ajax2@gmail.com",
+        "password": "qwerty",
+        "tokens": []
+    }
+    ]
 }
 
 
@@ -16,24 +24,58 @@ PATHWORDS = {
 def login():
     """Login function"""
 
+    current_user = None
     request_json = request.get_json(force=True)
-    login = request_json.get("email")
+    email = request_json.get("email")
     password = request_json.get("password")
-    if login not in LOGINS:
-        return {"error": "This login isn't exist"}, 400
-    if PATHWORDS.get(login) != password:
-        return {"error": "Invalid password"}, 400
-    return {"token": str(random.randint(100000000000000000, 1000000000000000000))}
+    for user in DATABASE["users"]:
+        if user["email"] == email:
+            current_user = user
+            break
+    if not current_user:
+        return {
+            "success": False,
+            "error_message": "This login isn't exist"
+        }, 400
+    if current_user["password"] != password:
+        return {
+            "success": False,
+            "error_message": "Invalid password"
+        }, 400
+    else:
+        token = random.randint(100000, 10000000)
+        return {
+            "success": True,
+            "token": token
+        }
+
 
 @app.route("/signup", methods=["POST"])
 def signup():
     """Sign Up function"""
-    return {"token": "LALALLALALAALLALA"}
+
+    request_json = request.get_json(force=True)
+    name = request_json.get("name")
+    email = request_json.get("email")
+    password = request_json.get("password")
+    DATABASE["users"].append({
+        "name": name,
+        "email": email,
+        "password": password,
+        "tokens": []
+    })
+    token = random.randint(100000, 10000000)
+    print(DATABASE)
+    return {
+        "success": True,
+        "token": token
+    }
+
 
 @app.route("/home", methods=["GET"])
 def home():
-    return {"text": "There some text"}
+    return {"text": "This page need to show your Tasks"}
 
 
 if __name__ == "__main__":
-   app.run(debug=True)
+    app.run(debug=True)
