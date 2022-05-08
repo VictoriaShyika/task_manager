5ff08cba5737d8ef50af37eb234922831e8d391b
VictoriaShyika <victoriashyika@gmail.com>
2022-05-08 13:59:42 +0300

    Task_Adding: Added components

--- client/src/components/TasksPage.js
+++ client/src/components/TasksPage.js
@@ -1,42 +1,32 @@
 import React from "react";
+import Button from "./UI/Button";
+import Input from "./UI/Input";
+import Textarea from "./UI/Textarea";
 
 const TasksPage = () => {
   return (
-    <div className="container" style={{background: "rgb(139, 185, 236)"}}>
-        
+    <div className="container" style={{ background: "rgb(139, 185, 236)" }}>
       <div className="py-3 rounded">
         <div className="row">
           <div className="col-md-2">
-            <button className="btn btn-dark m-3">+</button>
+            <Button className="btn btn-dark m-3">+</Button>
           </div>
           <div className="col-md-10">
-          <p className=" text-center text-uppercase">
-              Add new task
-            </p>
+            <p className=" text-center text-uppercase">Add new task</p>
           </div>
         </div>
       </div>
-      
-      <form  className="m-3">
-            <div className="input-group mb-3">
-              <input
-                type="text"
-                className="form-control"
-                placeholder="Task Title"
-              />
-            </div>
 
-            <div className="input-group mb-3">
-              <textarea
-                type="text"
-                className="form-control"
-                placeholder="Task Description"
-              />
-            </div>
-            <button type="submit" className="btn btn-primary">
-              Submit
-            </button>
-          </form>
+      <form className="m-3">
+        <div className="input-group mb-3">
+          <Input type="text" placeholder="Task Title" />
+        </div>
+
+        <div className="input-group mb-3">
+          <Textarea type="textarea" placeholder="Task Description" />
+        </div>
+        <Button type="submit">Submit</Button>
+      </form>
     </div>
   );
 };
---
+++ client/src/components/UI/Textarea.js
@@ -0,0 +1,5 @@
+const Textarea = (props) => {
+    return <textarea className="form-control form-control-lg bg-dark text-white" {...props} />;
+  };
+  export default Textarea;
+  
